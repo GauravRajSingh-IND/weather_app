a08f18e6fdf3272de0728bea8a7588bb7dd70efa
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-10 19:31:25 +0530

    function to get weather data of a given location completed

--- weather_function.py
+++ weather_function.py
@@ -1,7 +1,10 @@
+import os
+import dotenv
 import requests
 import geocoder
 from geopy.geocoders import Nominatim
 
+dotenv.load_dotenv()
 
 def get_location(address: str= None) -> tuple:
     """
@@ -36,3 +39,43 @@ def get_location(address: str= None) -> tuple:
     except Exception as e:
         print(f"Error occurred: {e}")
         return 26.9196, 75.7878
+
+
+#TODO: get weather data using location of the user
+def get_weather(latitude: float, longitude: float, mode: str="json", units: str="metric", lang: str="en") -> dict:
+    """
+    Get the weather data of a given (latitude and longitude) address.
+    :param latitude: latitude of the location
+    :param longitude: longitude of the location
+    :param mode: output format, default is json format
+    :param units: output temperature units, default is metric, i.e. Celsius.
+    :param lang: language of output, default is english
+    :return:
+    """
+
+    # prepare the parameters for the api request.
+    params = {
+        'lat':latitude,
+        'lon':longitude,
+        'appid': os.getenv('weather_api_key'),
+        'mode':mode,
+        'units':units,
+        'lang':lang
+            }
+
+    # Handle the API endpoint retrieval
+    endpoint = os.getenv('weather_end_point')
+    if not endpoint:
+        raise ValueError("Weather API endpoint is not set in environment variables.")
+
+    try:
+        # Call the API
+        response = requests.get(url=endpoint, params=params)
+        response.raise_for_status()  # Raise an error for bad responses
+
+        # Return the JSON response
+        return response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while fetching weather data: {e}")
+        return {"error": str(e)}
